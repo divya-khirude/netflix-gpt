e7f92f7ead68e75ca0490f783417fdaa68441138
Divya Khirude <dkhirude@gmail.com>
2024-09-26 20:14:03 +0530

    Episode-01: Part 2 (Added Sign Up Form)

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
   return (
     <div>
       <Header />
@@ -12,21 +18,34 @@ const Login = () => {
         />
       </div>
       <form className="w-[33%] absolute p-8 bg-black my-16 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
-        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-2 my-2 w-full bg-gray-800 bg-opacity-50 rounded-lg"
+          />
+        )}
         <input
           type="text"
           placeholder="Email Address"
-          className="p-4 my-2 w-full bg-gray-800 bg-opacity-50 rounded-lg"
+          className="p-2 my-2 w-full bg-gray-800 bg-opacity-50 rounded-lg"
         />
         <input
           type="password"
           placeholder="Password"
-          className="p-4 my-2 w-full bg-gray-800 bg-opacity-50 rounded-lg"
+          className="p-2 my-2 w-full bg-gray-800 bg-opacity-50 rounded-lg"
         />
-        <button className="p-4 my-4 bg-red-700 w-full rounded-lg">
-          Sign In
+        <button className="p-2 my-4 bg-red-700 w-full rounded-lg">
+          {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
-        <p className="py-4">New to Netflix? Sign Up Now</p>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already registered? Sign In Now"}
+        </p>
       </form>
     </div>
   );
